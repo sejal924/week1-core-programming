05cad1a029fa7cf6f247ca5fe03c4ce9c9e23faf
sejal924 <sejalchouhan24@gmail.com>
2025-01-11 16:38:34 +0530

    Findin age of harry

---
+++ level-1/AgeOfHarry.java
@@ -0,0 +1,20 @@
+//finding the age of harry
+import java.util.*;
+
+//creating the  AgeOfHarry class
+public class AgeOfHarry
+{
+    public static void main(String [] args)
+    {      
+	      // creating the variable birthYear , currentYear
+           int birthYear = 2000;
+           int currentYear=2024;
+		   
+		   //calculating harry age 
+           int harryAge = currentYear - birthYear; 
+		   
+		   // Printing the age of harry.
+           System.out.print("Harry's age in 2024 is: "+ harryAge);
+    }
+  
+}
