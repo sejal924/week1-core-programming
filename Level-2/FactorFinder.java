d55bdd341a41c28c3fa2b37b798e68b9a974b084
sejal924 <sejalchouhan24@gmail.com>
2025-01-13 20:54:48 +0530

    Method to find factors ,sum of factor ,product of factor and sum of square of factor

---
+++ Level-2/FactorFinder.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class FactorFinder {
+    
+	public static int[] findFactors(int number) {
+        int count = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                factors[index] = i;
+				index++;
+            }
+        }
+        return factors;
+    }
+
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int i = 0; i < factors.length; i++) {
+            sum += factors[i];
+        }
+        return sum;
+    }
+
+    public static int productOfFactors(int[] factors) {
+        int product = 1;
+        for (int i = 0; i < factors.length; i++) {
+            product *= factors[i];
+        }
+        return product;
+    }
+
+    public static int sumOfSquaresOfFactors(int[] factors) {
+        int sumOfSquares = 0;
+        for (int i = 0; i < factors.length; i++) {
+            sumOfSquares += Math.pow(factors[i], 2);
+        }
+        return sumOfSquares;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int[] factors = findFactors(number);
+
+        System.out.print("Factors of " + number + ": ");
+        for (int i = 0; i < factors.length; i++) {
+            System.out.print(factors[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Sum of factors: " + sumOfFactors(factors));
+        System.out.println("Product of factors: " + productOfFactors(factors));
+        System.out.println("Sum of squares of factors: " + sumOfSquaresOfFactors(factors));
+    }
+}
