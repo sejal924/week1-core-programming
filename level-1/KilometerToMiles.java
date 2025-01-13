e6aa524f1ef5a2372f6bdb0edd69d2867ffc7ecf
sejal924 <sejalchouhan24@gmail.com>
2025-01-13 15:17:45 +0530

    converting the distance from kilometer to miles

---
+++ level-1/KilometerToMiles.java
@@ -0,0 +1,16 @@
+//converting the distance from kilometer to miles
+import java.util.*;
+
+//creating the KilometerToMiles class
+public class KilometerToMiles
+{
+     public static void main(String [] args)
+     { 
+	      //creating the variable kilometer , mile
+          double kilometer = 10.8;
+		  // converting the kilometer to mile
+          double mile = 10.8/1.6;
+		  //printing the distance in miles
+          System.out.print("The distance" + kilometer + "km in miles is " + mile);
+     }
+}
